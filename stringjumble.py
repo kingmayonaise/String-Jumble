88220ba88ae32d0f48de7cffdfff935d93d8724d
kingmayonaise <danielmelnikov@hanovernorwichschools.org>
2016-10-04 18:51:20 -0400

    Updated from Brython Server: 10/4/2016 6:51:19 PM

--- stringjumble.py
+++ stringjumble.py
@@ -26,7 +26,7 @@ handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
 sentence=input("Please enter a string of text (the bigger the better): ")
-print('You entered '+'"'+sentence+'"'+' Now jumble it x:')
+print('You entered '+'"'+sentence+'"'+' Now jumble it:')
 #Putting the whole sentence in backwards order
 backwards=sentence[::-1]
 print(backwards)
